506b0d28a3160bdce0126c5b8a5dade63f956346
CuongTranXuan <35216837+CuongTranXuan@users.noreply.github.com>
2023-02-20 11:05:20 +0000

    polish login page

--- src/views/auth/Login.svelte
+++ src/views/auth/Login.svelte
@@ -2,9 +2,8 @@
   import { link } from "svelte-routing";
 
   // core components
-  const github = "../assets/img/github.svg";
-  const google = "../assets/img/google.svg";
-  export let location;
+
+  // export let location;
 </script>
 
 <div class="container mx-auto px-4 h-full">
@@ -14,33 +13,8 @@
         class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0"
       >
         <div class="rounded-t mb-0 px-6 py-6">
-          <div class="text-center mb-3">
-            <h6 class="text-blueGray-500 text-sm font-bold">
-              Sign in with
-            </h6>
-          </div>
-          <div class="btn-wrapper text-center">
-            <button
-              class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-              type="button"
-            >
-              <img alt="..." class="w-5 mr-1" src="{github}" />
-              Github
-            </button>
-            <button
-              class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-              type="button"
-            >
-              <img alt="..." class="w-5 mr-1" src="{google}" />
-              Google
-            </button>
-          </div>
-          <hr class="mt-6 border-b-1 border-blueGray-300" />
         </div>
         <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
-          <div class="text-blueGray-400 text-center mb-3 font-bold">
-            <small>Or sign in with credentials</small>
-          </div>
           <form>
             <div class="relative w-full mb-3">
               <label
@@ -71,18 +45,6 @@
                 placeholder="Password"
               />
             </div>
-            <div>
-              <label class="inline-flex items-center cursor-pointer">
-                <input
-                  id="customCheckLogin"
-                  type="checkbox"
-                  class="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
-                />
-                <span class="ml-2 text-sm font-semibold text-blueGray-600">
-                  Remember me
-                </span>
-              </label>
-            </div>
 
             <div class="text-center mt-6">
               <button
@@ -96,16 +58,6 @@
         </div>
       </div>
       <div class="flex flex-wrap mt-6 relative">
-        <div class="w-1/2">
-          <a href="#pablo" on:click={(e) => e.preventDefault()} class="text-blueGray-200">
-            <small>Forgot password?</small>
-          </a>
-        </div>
-        <div class="w-1/2 text-right">
-          <a use:link href="/auth/register" class="text-blueGray-200">
-            <small>Create new account</small>
-          </a>
-        </div>
       </div>
     </div>
   </div>
